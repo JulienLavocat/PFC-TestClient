b4757cc9d64da326f2e51b30edbee97fde28e664
JulienL <julien.lavocat@gmail.com>
2018-07-27 03:34:41 +0200

    Initial commit
    Initial commit, working version 1.0

---
+++ src/Program.cs
@@ -0,0 +1,86 @@
+﻿using LiteNetLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static NetPeer server;
+	public static NetManager client;
+	public static Guid clientId;
+	public static bool running = true;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Starting PFC client.");
+        EventBasedNetListener listener = new EventBasedNetListener();
+		client = new NetManager(listener, "pfcMasterserver");
+        client.Start();
+        //server = client.Connect("localhost", 9100);
+		
+        listener.NetworkReceiveEvent += (fromPeer, dataReader) =>
+        {
+			switch (dataReader.GetByte())
+			{
+				case 0:	//Login confirm
+					clientId = new Guid(dataReader.GetRemainingBytes());
+					Console.WriteLine("Assigned client uuid is : " + clientId);
+					break;
+
+				case 1:	//Match found
+
+					break;
+			}
+        };
+
+		Thread t = new Thread(PollConsole);
+		t.Start();
+
+        while (running)
+        {
+            client.PollEvents();
+            Thread.Sleep(150);
+        }
+
+        client.Stop();
+
+    }
+
+	static void PollConsole()
+	{
+		while(running)
+		{
+			Console.Write("Command > ");
+			string[] args = Console.ReadLine().Split(' ');
+
+			if(args[0] == "stop")
+			{
+				Console.WriteLine("Stopping client...");
+				running = false;
+			}
+			
+			if(args[0] == "connect")
+			{
+				Console.WriteLine("Connecting to masterserver...");
+				server = client.Connect("localhost", 9100);
+				Thread.Sleep(1000);
+				Console.WriteLine("Connection state : " + server.ConnectionState);
+			}
+
+			if(args[0] == "join")
+			{
+				bool ranked = (args[1] == "r");
+				if (ranked)
+					Console.WriteLine("Joining ranked match.");
+				else
+					Console.WriteLine("Joining normal match.");
+				server.Send(PClient.JoinQueue(ranked), SendOptions.ReliableOrdered);
+			}
+
+		}
+	}
+
+}
---
+++ src/packets/client/PClient.cs
@@ -0,0 +1,18 @@
+﻿using LiteNetLib.Utils;
+using System;
+
+class PClient
+{
+
+    public static NetDataWriter JoinQueue(bool ranked)
+    {
+        NetDataWriter writer = new NetDataWriter();
+		byte id = 1;
+		byte mode = Convert.ToByte(ranked);
+        writer.Put(id);
+		writer.Put(mode);
+		Console.WriteLine("Mode id is " + mode);
+        return writer;
+    }
+
+}
